f821972695b82aeb84a2bb0a61ca93791a68a677
sanghuncho <moondrive@hotmail.co.kr>
2020-12-29 08:17:27 +0100

    initial commit for apis

---
+++ src/main/java/coupang/coupangApi.java
@@ -0,0 +1,315 @@
+package coupang;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class coupangApi {
+    private static final String HOST = "api-gateway.coupang.com";
+    private static final int PORT = 443;
+    private static final String SCHEMA = "https";
+    //replace with your own accessKey
+    //private static final String ACCESS_KEY = "847a90a8-92b2-4f1e-925c-e8668af62b45";
+    private static String ACCESS_KEY;
+    //replace with your own secretKey
+    //private static final String SECRET_KEY = "3d0b21dec50684acdc500a00fd05c652eb111617";
+    private static String SECRET_KEY;
+    private static String VENDOR;
+    
+    private static final String userDir = System.getProperty("user.dir");
+    private static final String configPath = userDir + "/src/main/resources/coupang/config.properties";
+
+    public static void main(String[] args) {
+        initCoupangApi();
+        
+        coupangApi hmacTest = new coupangApi();
+        
+        hmacTest.getDeliveryStartPlace();
+        //hmacTest.getReturnPlace();
+        //hmacTest.getCategoryMetas("56163");
+    }
+    
+    private static void initCoupangApi() {
+        try (InputStream input = new FileInputStream(configPath)) {
+
+            Properties prop = new Properties();
+
+            prop.load(input);
+
+            //load a properties file from class path, inside static method
+            prop.load(input);
+
+            //get the property value and print it out
+            //System.out.println(prop.getProperty("coupang.accessKey"));
+            ACCESS_KEY = prop.getProperty("coupang.accessKey");
+            SECRET_KEY = prop.getProperty("coupang.secretKey");
+            VENDOR = prop.getProperty("coupang.vendor");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void getCategoryMetas(String categoryId) {
+        //params
+        String method = "GET";
+        String path = "/v2/providers/seller_api/apis/api/v1/marketplace/meta/category-related-metas/display-category-codes/" + categoryId;
+
+        CloseableHttpClient client = null;
+        try {
+            //create client
+            client = HttpClients.createDefault();
+            //build uri
+            URIBuilder uriBuilder = new URIBuilder()
+                    .setPath(path);
+
+            /********************************************************/
+            //authorize, demonstrate how to generate hmac signature here
+                //String authorization = Hmac.generate(method, uriBuilder.build().toString(), SECRET_KEY, ACCESS_KEY);
+            //print out the hmac key
+            //System.out.println(authorization);
+            /********************************************************/
+
+                //uriBuilder.setScheme(SCHEMA).setHost(HOST).setPort(PORT);
+            uriBuilder.setScheme(SCHEMA).setHost(HOST);
+            HttpGet get = new HttpGet(uriBuilder.build().toString());
+            /********************************************************/
+            // set header, demonstarte how to use hmac signature here
+                //get.addHeader("Authorization", authorization);
+            /********************************************************/
+            get.addHeader("content-type", "application/json");
+            CloseableHttpResponse response = null;
+            try {
+                //execute get request
+                response = client.execute(get);
+                //print result
+                System.out.println("status code:" + response.getStatusLine().getStatusCode());
+                System.out.println("status message:" + response.getStatusLine().getReasonPhrase());
+                HttpEntity entity = response.getEntity();
+                System.out.println("result:" + EntityUtils.toString(entity));
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (response != null) {
+                    try {
+                        response.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public void getDeliveryStartPlace() {
+        //params
+        String method = "GET";
+        String path = "/v2/providers/marketplace_openapi/apis/api/v1/vendor/shipping-place/outbound";
+
+        CloseableHttpClient client = null;
+        try {
+            //create client
+            client = HttpClients.createDefault();
+            //build uri
+            URIBuilder uriBuilder = new URIBuilder()
+                    .setPath(path)
+                    .addParameter("pageSize", "1")     
+                    .addParameter("pageNum", "1") ;
+
+            /********************************************************/
+            //authorize, demonstrate how to generate hmac signature here
+                //String authorization = Hmac.generate(method, uriBuilder.build().toString(), SECRET_KEY, ACCESS_KEY);
+            //print out the hmac key
+            //System.out.println(authorization);
+            /********************************************************/
+
+            //uriBuilder.setScheme(SCHEMA).setHost(HOST).setPort(PORT);
+            uriBuilder.setScheme(SCHEMA).setHost(HOST);
+            HttpGet get = new HttpGet(uriBuilder.build().toString());
+            /********************************************************/
+            // set header, demonstarte how to use hmac signature here
+                //get.addHeader("Authorization", authorization);
+            /********************************************************/
+            get.addHeader("content-type", "application/json");
+            CloseableHttpResponse response = null;
+            try {
+                //execute get request
+                response = client.execute(get);
+                //print result
+                System.out.println("status code:" + response.getStatusLine().getStatusCode());
+                System.out.println("status message:" + response.getStatusLine().getReasonPhrase());
+                HttpEntity entity = response.getEntity();
+                System.out.println("result:" + EntityUtils.toString(entity));
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (response != null) {
+                    try {
+                        response.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public void getReturnPlace() {
+        //params
+        String method = "GET";
+        String path = "/v2/providers/openapi/apis/api/v4/vendors/A00334178/returnShippingCenters";
+
+        CloseableHttpClient client = null;
+        try {
+            //create client
+            client = HttpClients.createDefault();
+            //build uri
+            URIBuilder uriBuilder = new URIBuilder()
+                    .setPath(path)
+                    .addParameter("pageSize", "10")     
+                    .addParameter("pageNum", "1") ;
+
+            /********************************************************/
+            //authorize, demonstrate how to generate hmac signature here
+                //String authorization = Hmac.generate(method, uriBuilder.build().toString(), SECRET_KEY, ACCESS_KEY);
+            //print out the hmac key
+                //System.out.println(authorization);
+            /********************************************************/
+
+            //uriBuilder.setScheme(SCHEMA).setHost(HOST).setPort(PORT);
+            uriBuilder.setScheme(SCHEMA).setHost(HOST);
+            HttpGet get = new HttpGet(uriBuilder.build().toString());
+            /********************************************************/
+            // set header, demonstarte how to use hmac signature here
+                //get.addHeader("Authorization", authorization);
+            /********************************************************/
+            get.addHeader("content-type", "application/json");
+            CloseableHttpResponse response = null;
+            try {
+                //execute get request
+                response = client.execute(get);
+                //print result
+                System.out.println("status code:" + response.getStatusLine().getStatusCode());
+                System.out.println("status message:" + response.getStatusLine().getReasonPhrase());
+                HttpEntity entity = response.getEntity();
+                System.out.println("result:" + EntityUtils.toString(entity));
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (response != null) {
+                    try {
+                        response.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public void post() {
+        //params
+        String method = "POST";
+        String path = "/v2/providers/seller_api/apis/api/v1/marketplace/seller-products";
+        
+        //replace with your own product registration json data
+        String strjson="{}";
+        
+        CloseableHttpClient client = null;
+        try {
+            //create client
+            client = HttpClients.createDefault();
+            //build uri
+            URIBuilder uriBuilder = new URIBuilder()
+                    .setPath(path);
+
+            /********************************************************/
+            //authorize, demonstrate how to generate hmac signature here
+                //String authorization = Hmac.generate(method, uriBuilder.build().toString(), SECRET_KEY, ACCESS_KEY);
+            //print out the hmac key
+                //System.out.println(authorization);
+            /********************************************************/
+
+            uriBuilder.setScheme(SCHEMA).setHost(HOST).setPort(PORT);
+            HttpPost requestPost = new HttpPost(uriBuilder.build().toString());
+
+            StringEntity params =new StringEntity(strjson,"UTF-8");
+            
+            /********************************************************/
+            // set header, demonstarte how to use hmac signature here
+                //requestPost.addHeader("Authorization", authorization);
+            /********************************************************/
+            requestPost.addHeader("content-type", "application/json");
+            requestPost.setEntity(params);
+            CloseableHttpResponse response = null;
+            try {
+                //execute post request
+                response = client.execute(requestPost);
+                //print result
+                System.out.println("status code:" + response.getStatusLine().getStatusCode());
+                System.out.println("status message:" + response.getStatusLine().getReasonPhrase());
+                HttpEntity entity = response.getEntity();
+                System.out.println("result:" + EntityUtils.toString(entity));
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (response != null) {
+                    try {
+                        response.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
